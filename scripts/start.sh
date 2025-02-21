ad469446d9d0fa4c512ee500b04310545c8d1c6e
Milan Kappen <milan.kappen@iu.org>
2025-02-21 10:25:15 +0100

    BREAKING CHANGE: pusblish initial version

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,32 +5,6 @@ set -e  # Exit the script if any statement returns a non-true return value
 #                          Function Definitions                                #
 # ---------------------------------------------------------------------------- #
 
-# Format text to fit in box with ellipsis if needed
-format_text() {
-    local text="$1"
-    local max_length=$2
-    if [ ${#text} -gt $max_length ]; then
-        echo "${text:0:$((max_length-3))}..."
-    else
-        printf "%-${max_length}s" "$text"
-    fi
-}
-
-# Format version string
-format_version() {
-    local version=$(cat /VERSION)
-    format_text "$version" 12
-}
-
-# Create centered text
-center_text() {
-    local text="$1"
-    local width=70  # Total width inside box
-    local text_length=${#text}
-    local padding=$(( (width - text_length) / 2 ))
-    printf "%${padding}s%s%*s" "" "$text" $((width - padding - text_length)) ""
-}
-
 # Start nginx service
 start_nginx() {
     echo "Starting Nginx service..."
@@ -54,17 +28,6 @@ setup_ssh() {
         mkdir -p ~/.ssh
         echo "$PUBLIC_KEY" >> ~/.ssh/authorized_keys
         chmod 700 -R ~/.ssh
-
-        # Configure welcome message
-        echo "+--------------------------------------------------------------------------+
-|                                                                          |
-|$(center_text "CONSISTENT CHARACTER SETUP")|
-|                                                                          |
-|  $(format_text "https://github.com/joyandmighty/consistent-character" 65)|
-|                                                                          |
-|  Version: $(format_version)                                              |
-|                                                                          |
-+--------------------------------------------------------------------------+" > /etc/motd
 
         if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
             ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -q -N ''
